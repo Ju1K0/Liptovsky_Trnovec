fa51d1db2b6c00ad5f90cdb030b466dcc4d02352
Ju1K0 <julius.kotkolik@web.de>
2026-09-23 14:00:09 +0200

    Marzipano Rundgang Upload

---
+++ data.js
@@ -0,0 +1,738 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-chodbaflur-1",
+      "name": "Chodba/Flur 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1472,
+      "initialViewParameters": {
+        "yaw": 3.1148871482753178,
+        "pitch": 0.10122669343312829,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.103534698080801,
+          "pitch": 0.1584184763563279,
+          "rotation": 0,
+          "target": "1-chodbaflur-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-chodbaflur-2",
+      "name": "Chodba/Flur 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1472,
+      "initialViewParameters": {
+        "yaw": -0.011413235713833458,
+        "pitch": 0.06667346772747429,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5045750201569703,
+          "pitch": 0.10909753459217164,
+          "rotation": 0,
+          "target": "5-spalnasz-1"
+        },
+        {
+          "yaw": -0.10377488007615554,
+          "pitch": 0.06897535418188383,
+          "rotation": 0,
+          "target": "3-sprchadusche"
+        },
+        {
+          "yaw": 0.6268785587959034,
+          "pitch": 0.04942984985663479,
+          "rotation": 0,
+          "target": "7-spalnasz-2"
+        },
+        {
+          "yaw": 2.0939931720654084,
+          "pitch": 0.043337291739666384,
+          "rotation": 0,
+          "target": "4-obyvackawz"
+        },
+        {
+          "yaw": 2.7972416101017163,
+          "pitch": -0.007539827944507138,
+          "rotation": 0,
+          "target": "8-schodytreppenhaus"
+        },
+        {
+          "yaw": 3.129950531303992,
+          "pitch": 0.3420796197736955,
+          "rotation": 0,
+          "target": "0-chodbaflur-1"
+        },
+        {
+          "yaw": -2.2717381509147767,
+          "pitch": 0.09570714655246704,
+          "rotation": 0,
+          "target": "2-kuchinakche"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-kuchinakche",
+      "name": "Kuchina/Küche",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1472,
+      "initialViewParameters": {
+        "yaw": 2.9042923501293316,
+        "pitch": 0.2984722777065123,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.7528492154227369,
+          "pitch": 0.12381605604329238,
+          "rotation": 0,
+          "target": "1-chodbaflur-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-sprchadusche",
+      "name": "Sprcha/Dusche",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1472,
+      "initialViewParameters": {
+        "yaw": -0.6879435284088835,
+        "pitch": 0.3141437840587411,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.0537763608074187,
+          "pitch": 0.5213928251140061,
+          "rotation": 0,
+          "target": "1-chodbaflur-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-obyvackawz",
+      "name": "Obyvacka/WZ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1472,
+      "initialViewParameters": {
+        "yaw": -1.2506847465110393,
+        "pitch": 0.1781215268903349,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.595458459039868,
+          "pitch": 0.13783457392941756,
+          "rotation": 0,
+          "target": "1-chodbaflur-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-spalnasz-1",
+      "name": "Spalna/SZ 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1472,
+      "initialViewParameters": {
+        "yaw": 2.090755993979373,
+        "pitch": -0.019750196266706155,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.0841832793930966,
+          "pitch": 0.012100432242435843,
+          "rotation": 0,
+          "target": "6-komoraabstellraum"
+        },
+        {
+          "yaw": -0.9756289536868721,
+          "pitch": -0.0030548576646278747,
+          "rotation": 0,
+          "target": "7-spalnasz-2"
+        },
+        {
+          "yaw": -1.1623638472925784,
+          "pitch": 0.011525022641041716,
+          "rotation": 0,
+          "target": "3-sprchadusche"
+        },
+        {
+          "yaw": -0.8705342379507446,
+          "pitch": 0.43770003403188973,
+          "rotation": 0,
+          "target": "1-chodbaflur-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-komoraabstellraum",
+      "name": "Komora/Abstellraum",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1472,
+      "initialViewParameters": {
+        "yaw": 0.4809942004134449,
+        "pitch": 0.07603438519550565,
+        "fov": 1.6614322194596747
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.210446577380523,
+          "pitch": -0.15984492783232795,
+          "rotation": 0,
+          "target": "5-spalnasz-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-spalnasz-2",
+      "name": "Spalna/SZ 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1472,
+      "initialViewParameters": {
+        "yaw": -0.11160487656737317,
+        "pitch": 0.7219316082599416,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.7496700661195703,
+          "pitch": -0.02496119301490829,
+          "rotation": 0,
+          "target": "5-spalnasz-1"
+        },
+        {
+          "yaw": -1.8803775162556544,
+          "pitch": 0.2879399301601957,
+          "rotation": 0,
+          "target": "1-chodbaflur-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-schodytreppenhaus",
+      "name": "Schody/Treppenhaus",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1472,
+      "initialViewParameters": {
+        "yaw": -2.787248151138961,
+        "pitch": 0.213276257028026,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.8474729567743395,
+          "pitch": 0.5774314668035796,
+          "rotation": 0,
+          "target": "1-chodbaflur-2"
+        },
+        {
+          "yaw": -3.1058315060794044,
+          "pitch": -0.3591247090103362,
+          "rotation": 0,
+          "target": "9-podkroviedg-1"
+        },
+        {
+          "yaw": -3.0161244670740484,
+          "pitch": -0.37127617144197345,
+          "rotation": 0,
+          "target": "11-podkroviedg-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-podkroviedg-1",
+      "name": "Podkrovie/DG 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1472,
+      "initialViewParameters": {
+        "yaw": 2.703849668872765,
+        "pitch": 0.46299512046598323,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0942307851040773,
+          "pitch": 0.2930082801580589,
+          "rotation": 0,
+          "target": "14-podkroviedb-kupelkabad"
+        },
+        {
+          "yaw": -2.9115397775664853,
+          "pitch": 0.19578063876410212,
+          "rotation": 0,
+          "target": "15-podkroviedg-komoraabstellraum"
+        },
+        {
+          "yaw": -2.5425429921575464,
+          "pitch": 0.2113437070484938,
+          "rotation": 0,
+          "target": "11-podkroviedg-2"
+        },
+        {
+          "yaw": -2.53981681288308,
+          "pitch": 0.03751578486788176,
+          "rotation": 0,
+          "target": "13-balkon"
+        },
+        {
+          "yaw": -2.2675478783599967,
+          "pitch": 0.12608706793821156,
+          "rotation": 0,
+          "target": "8-schodytreppenhaus"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-podkroviedg-balkon-1",
+      "name": "Podkrovie/DG Balkon 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1472,
+      "initialViewParameters": {
+        "yaw": 0.47223027878995794,
+        "pitch": -0.09417898924832002,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-podkroviedg-2",
+      "name": "Podkrovie/DG 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1472,
+      "initialViewParameters": {
+        "yaw": 2.6958122753018117,
+        "pitch": 0.22224574336940428,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.9553161166009794,
+          "pitch": 0.19717329944390194,
+          "rotation": 0,
+          "target": "8-schodytreppenhaus"
+        },
+        {
+          "yaw": 0.3588758283914881,
+          "pitch": 0.21710127445167515,
+          "rotation": 0,
+          "target": "14-podkroviedb-kupelkabad"
+        },
+        {
+          "yaw": 1.5406631225932097,
+          "pitch": 0.16982617595962068,
+          "rotation": 0,
+          "target": "15-podkroviedg-komoraabstellraum"
+        },
+        {
+          "yaw": -0.05371137876341869,
+          "pitch": 0.21585916697705976,
+          "rotation": 0,
+          "target": "9-podkroviedg-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-podkroviedg-balkon-2",
+      "name": "Podkrovie/DG Balkon 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1472,
+      "initialViewParameters": {
+        "yaw": 0.04311894953163353,
+        "pitch": 0.03475213112378306,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.0905250850697712,
+          "pitch": 0.8796938655871784,
+          "rotation": 0,
+          "target": "13-balkon"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "13-balkon",
+      "name": "Balkon",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1472,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.7829736830451566,
+          "pitch": 0.5709312884274471,
+          "rotation": 0,
+          "target": "12-podkroviedg-balkon-2"
+        },
+        {
+          "yaw": -2.7248271581824444,
+          "pitch": 0.025444991579215426,
+          "rotation": 0,
+          "target": "11-podkroviedg-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "14-podkroviedb-kupelkabad",
+      "name": "Podkrovie/DB Kupelka/Bad",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1472,
+      "initialViewParameters": {
+        "yaw": -0.4243565072841502,
+        "pitch": 0.07974658652210209,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.8232941851604263,
+          "pitch": 0.4878391613254589,
+          "rotation": 0,
+          "target": "9-podkroviedg-1"
+        },
+        {
+          "yaw": -1.7590551354483672,
+          "pitch": 0.09986606191493763,
+          "rotation": 0,
+          "target": "8-schodytreppenhaus"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "15-podkroviedg-komoraabstellraum",
+      "name": "Podkrovie/DG Komora/Abstellraum",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1472,
+      "initialViewParameters": {
+        "yaw": -0.11999872531788824,
+        "pitch": 0.43550944629173216,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.9603613859704083,
+          "pitch": 0.14538059732480768,
+          "rotation": 0,
+          "target": "11-podkroviedg-2"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Liptovsky Trnovec",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
